0a1dc8d6cd51ece5ba71fdab7c7d2c521e1369d9
EfficcioAdmin <efficio4@gmail.com>
2024-09-29 13:02:13 +0800

    Initial Commit

---
+++ client/src/Routes/Admin/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navigation from './Navigation.tsx';
+import './Dashboard.scss'
+
+export default function Dashboard({className})
+{
+	return (
+		<div className={`dashboard`}>
+			<div className="main-dashboard">
+				DASHBOARD
+			</div>
+		</div>
+	);
+}
